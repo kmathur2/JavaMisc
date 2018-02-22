73e72c4c1c10d1537a35d098be455fcdea78a994
Kartik Mathur <kartik@bluedata.com>
2018-02-21 20:48:37 -0800

    some readability changes

--- LeetCode/LongestSubstring.java
+++ LeetCode/LongestSubstring.java
@@ -4,14 +4,16 @@
 
 /*
 This is dynamic programming problem, so the way I am doing it is
+example -
+
    f g d d d l d
 d  0 0 1 1 1 0 1
 d  0 0 1 2 2 0 1
-d  0 0 1 2 3 0 1 
+d  0 0 1 2 3 0 1
  */
 public class LongestSubstring {
     public static void main(String args[]){
-        System.out.print(LongestSub("dddd", "fgdddll"));
+        System.out.print(LongestSub("my name is ho lacko bum bum", "but my name is hothing bum bum"));
     }
 
     static int LongestSub(String s1, String s2){
@@ -23,19 +25,43 @@ public class LongestSubstring {
         if(s1 == "" || s2 == ""){
             return 0;
         }
-        for(i=0;i<m;i++){
-            for(j=0;j< n; j++) {
+
+        /*line 27-45 is useless, filling 1st row
+        and 1st column to avoid confusing if-else in the
+        main loop**/
+
+        //starts here
+        if(s1.charAt(0)==s2.charAt(0)){
+            L[0][0]=1;
+        }else{
+            L[0][0]=0;
+        }
+        for(int row=1;row<m;row++){
+            if(s1.charAt(row) == s2.charAt(0)){
+                L[row][0]=L[row-1][0]+1;
+            }else{
+                L[row][0] = L[row-1][0];
+            }
+        }
+        for(int col=0;col<n;col++){
+            if(s1.charAt(0) == s2.charAt(col)){
+                L[0][col]=1;
+            }else{
+                L[0][col]=0;
+            }
+        }
+        //ends here
+
+        //main logic starts here, filling 1st row and
+        //column has been taken care of earlier
+
+        for(i=1;i<m;i++){
+            for(j=1;j< n; j++) {
                 if (s1.charAt(i) == s2.charAt(j)) {
-                    if (i > 0 && j > 0) {
-                        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                            L[i][j] = L[i - 1][j - 1] + 1;
-                        }else {
-                            L[i][j]=1;
-                        }
-                    }else if(i==0) {
-                        L[i][j]=1;
+                    if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                        L[i][j] = L[i - 1][j - 1] + 1;
                     }else {
-                        L[i][j] = 1 + L[i-1][j];
+                        L[i][j]=1;
                     }
                 }else {
                     L[i][j] = 0;
